4661bd76ab9c40422bda7f538897b608ea581ef5
JessieDev1996 <bankeiei2215@gmail.com>
2022-09-18 19:03:37 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  var app = MaterialApp(
+    title: "My App",
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Hello FLutter ครับ"),
+      ),
+    ),
+  );
+  runApp(app);
+}
