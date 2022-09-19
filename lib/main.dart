2d6df6efd6dbb085875176f648fc4a7abb84592d
JessieDev1996 <bankeiei2215@gmail.com>
2022-09-19 22:18:00 +0700

    ListPicture FoodMenu

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'FoodMenu.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,6 +22,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<FoodMenu> menu = [
+    FoodMenu("กุ้งเผา", "500", "assets/images/picture1.jpg"),
+    FoodMenu("กระเพราหมู", "80", "assets/images/picture2.jpg"),
+    FoodMenu("ส้มตำ", "60", "assets/images/picture3.jpg"),
+  ];
   int number = 0;
   @override
   Widget build(BuildContext context) {
@@ -28,36 +34,42 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text("Hello FLutter ครับ"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            Text("สวัสดีครับทุกคน"),
-            Text("Hello Dart"),
-            Text("Hello Flutter"),
-            Text("กดปุ่มเพื่อเพิ่มจำนวนตัวเลข"),
-            Text(
-              number.toString(),
-              style: TextStyle(fontSize: 60),
-            ),
-            Image(
-                image: NetworkImage(
-                    "https://img.freepik.com/premium-photo/funny-cat-sunglasses-cat-with-glasses-light-blue-clean-sunny-background_90380-2753.jpg"))
-          ],
-        ),
-      ),
+      body: ListView.builder(
+          itemCount: menu.length,
+          itemBuilder: (BuildContext context, index) {
+            FoodMenu food = menu[index];
+            return ListTile(
+              leading: Image.asset(food.img),
+              title: Text(
+                food.name,
+                style: TextStyle(fontSize: 30),
+              ),
+            );
+          }),
       floatingActionButton: FloatingActionButton(
-        onPressed: (() {
-          addNumber();
-        }),
+        onPressed: (() {}),
         child: Icon(Icons.add),
       ),
     );
   }
 
-  void addNumber() {
-    setState(() {
-      number++;
-    });
+  List<Widget> getData(int count) {
+    List<Widget> data = [];
+    data.add(Text("กดปุ่มเพื่อเพิ่มจำนวนเลข"));
+    data.add(Text(
+      number.toString(),
+      style: TextStyle(fontSize: 60),
+    ));
+    data.add(Image(
+        image: NetworkImage(
+            "https://img.freepik.com/premium-photo/funny-cat-sunglasses-cat-with-glasses-light-blue-clean-sunny-background_90380-2753.jpg")));
+    for (var i = 0; i < 10; i++) {
+      var menu = ListTile(
+        title: Text("เมนู $i"),
+        subtitle: Text("หัวข้อย่อย ${i + 1}"),
+      );
+      data.add(menu);
+    }
+    return data;
   }
 }
